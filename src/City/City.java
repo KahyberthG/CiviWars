aacb3e61eb15e621b779016d275950c39e6d33da
Kahyberthst <kahyberth.gonzalez@correounivalle.edu.co>
2023-03-14 23:28:54 -0500

    some basic structure

---
+++ src/City/City.java
@@ -0,0 +1,11 @@
+package resources;
+
+public class City {
+
+    private Tile tile;
+    private int population;
+    private int food;
+    private int production;
+    private
+
+}
---
+++ src/units/Units.java
@@ -0,0 +1,18 @@
+package resources;
+
+public class Units {
+    private int health;
+    private int movement;
+
+    private Tile tile;
+
+    private UnitType type;
+
+    public Units() {
+
+    }
+
+
+}
+
+
